4a0d89c3a68a5125d2066e2dc7202f7274ff2400
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2022-07-28 15:37:00 +0700

    .

--- hm_laravel_35_Download_All_Source/index.js
+++ hm_laravel_35_Download_All_Source/index.js
@@ -4,8 +4,8 @@ const {exec} = require('child_process')
 // const websiteUrl = 'https://stackoverflow.com/';
 // const websiteUrl = 'https://hieunm47.github.io/';
 // const websiteUrl = 'https://www.huytruong.design/';
-const websiteUrl = 'https://tinhte.vn/';
-var directoryPath = 'tinhte-'+Date.now();
+const websiteUrl = 'https://thucphamtuyetlinh.com/';
+var directoryPath = 'thucphamtuyetlinh-'+Date.now();
 
 
 exec(`node-site-downloader download -s ${websiteUrl} -d ${websiteUrl} -o ${directoryPath} -v --include-images`,(err,stdout,stderr) =>{
